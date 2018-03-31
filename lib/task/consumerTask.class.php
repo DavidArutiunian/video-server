26b9c7747a1f025ba75083f9c9ee828cd8bace2b
David Arutiunian <arutunjan666@mail.com>
2018-03-31 22:17:26 +0300

    remove multiple try/catch blocks

--- lib/task/consumerTask.class.php
+++ lib/task/consumerTask.class.php
@@ -52,11 +52,20 @@ class consumerTask extends sfBaseTask
     {
         $this->callback = function ($message) {
             $videoFile = $this->getVideoFile($message->body);
-            $this->execFFMpeg($videoFile);
-            $this->setVideoState($videoFile, EVideoFileStates::READY);
+            try {
+                $this->execFFMpeg($videoFile);
+                $this->setVideoState($videoFile, EVideoFileStates::READY);
+            } catch (PropelException $e) {
+                $this->onError($videoFile);
+                error_log($e->getMessage());
+            }
         };
     }
 
+    /**
+     * @param VideoFile $videoFile
+     * @throws PropelException
+     */
     private function execFFMpeg(VideoFile $videoFile): void
     {
         $relativePathToFile = $videoFile->getDir() . '/' . $videoFile->getFilename();
@@ -65,32 +74,23 @@ class consumerTask extends sfBaseTask
          * @var IFFMpeg $ffmpeg
          */
         $ffmpeg = new FFMpeg($pathToFile);
-        try {
-            $duration = $ffmpeg->getDuration();
-            $this->setDuration($videoFile, $duration);
-            $size = $ffmpeg->getSize();
-            $this->setSize($videoFile, $size);
-            $ffmpeg->generateThumbs($videoFile);
-        } catch (Error $e) {
-            $this->onError($videoFile);
-            error_log($e->getMessage());
-        } catch (PropelException $e) {
-            $this->onError($videoFile);
-            error_log($e->getMessage());
-        }
-        return;
+        $duration = $ffmpeg->getDuration();
+        $this->setDuration($videoFile, $duration);
+        $size = $ffmpeg->getSize();
+        $this->setSize($videoFile, $size);
+        $ffmpeg->generateThumbs($videoFile);
     }
 
+    /**
+     * @param VideoFile $videoFile
+     * @param string $state
+     * @throws PropelException
+     */
     private function setVideoState(VideoFile $videoFile, string $state = EVideoFileStates::READY): void
     {
         $videoFile->setState(array_search($state, VideoFile::getStates()));
-        try {
-            $videoFile->save();
-        } catch (PropelException $e) {
-            $this->onError($videoFile);
-            error_log($e->getMessage());
-        }
-        return;
+        $videoFile->save();
+        $this->onError($videoFile);
     }
 
     private function getVideoFile(int $id): VideoFile
@@ -98,30 +98,32 @@ class consumerTask extends sfBaseTask
         return VideoFilePeer::retrieveByPK($id);
     }
 
+    /**
+     * @param VideoFile $videoFile
+     * @param float $duration
+     * @throws PropelException
+     */
     private function setDuration(VideoFile $videoFile, float $duration): void
     {
         $videoFile->setDuration($duration);
-        try {
-            $videoFile->save();
-        } catch (PropelException $e) {
-            $this->onError($videoFile);
-            error_log($e->getMessage());
-        }
-        return;
+        $videoFile->save();
     }
 
+    /**
+     * @param VideoFile $videoFile
+     * @param int $size
+     * @throws PropelException
+     */
     private function setSize(VideoFile $videoFile, int $size): void
     {
         $videoFile->setSize($size);
-        try {
-            $videoFile->save();
-        } catch (PropelException $e) {
-            $this->onError($videoFile);
-            error_log($e->getMessage());
-        }
-        return;
+        $videoFile->save();
     }
 
+    /**
+     * @param VideoFile $videoFile
+     * @throws PropelException
+     */
     private function onError(VideoFile $videoFile): void
     {
         $this->setVideoState($videoFile, EVideoFileStates::ERROR);
